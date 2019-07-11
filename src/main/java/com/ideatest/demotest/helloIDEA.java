826d926450a289833d7920197c42cf9d02334073
haoyingjie <hao_ying_jie@163.com>
2019-07-11 14:37:46 +0800

    提交配置文件

--- src/main/java/com/ideatest/demotest/helloIDEA.java
+++ src/main/java/com/ideatest/demotest/helloIDEA.java
@@ -1,7 +1,17 @@
 package com.ideatest.demotest;
 
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class helloIDEA {
     public static void main(String[] args){
         System.out.println("hello IDEA!");
+    }
+
+    public void mapDemoTest(){
+        Map<String , Object> map = new HashMap<String, Object>();
+        JSONObject jsobj = new JSONObject();
     }
 }
