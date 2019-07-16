b27d8ef239cc8a0ff7cfdc6bc8c3ea4919349ef5
haoyingjie <hao_ying_jie@163.com>
2019-07-16 15:34:28 +0800

    0716提交文件

--- src/main/java/com/ideatest/demotest/helloIDEA.java
+++ src/main/java/com/ideatest/demotest/helloIDEA.java
@@ -7,11 +7,19 @@ import java.util.Map;
 
 public class helloIDEA {
     public static void main(String[] args){
+
         System.out.println("hello IDEA!");
+        helloIDEA helloObj = new helloIDEA();
+        helloObj.mapDemoTest();
     }
 
     public void mapDemoTest(){
         Map<String , Object> map = new HashMap<String, Object>();
         JSONObject jsobj = new JSONObject();
+        String jsonStringObj = "";
+        jsobj.put("name","IDEA");
+        jsobj.put("colour","white");
+        String jsobjString = jsobj.toString();
+        System.out.println(jsobjString);
     }
 }
